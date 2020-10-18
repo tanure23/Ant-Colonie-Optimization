1671d83879442b7ac8f41adff6d1f85261416147
Breno Tanure <brenotpr@gmail.com>
2020-10-18 15:12:17 -0300

    Updated maxAnts experiment ant params

--- exec_maxAnts.sh
+++ exec_maxAnts.sh
@@ -17,14 +17,14 @@ mkdir logs -p
 
 for counter in $(seq 1 10) 
 do
-    echo "Running exp on maxAnts: 5 || Entrada2 || Counter: $counter"
-    python3 main.py --input "bases_grafos/entrada2.txt" --maxAnts 5 --id "maxAnts-5-entrada2-$counter" > logs/results-maxAnts-entrada2-maxAnts-5-$counter-run.log
+    echo "Running exp on maxAnts: 50 || Entrada2 || Counter: $counter"
+    python3 main.py --input "bases_grafos/entrada2.txt" --maxAnts 50 --id "maxAnts-50-entrada2-$counter" > logs/results-maxAnts-entrada2-maxAnts-50-$counter-run.log
 
-    echo "Running exp on maxAnts: 10 || Entrada2 || Counter: $counter"
-    python3 main.py --input "bases_grafos/entrada2.txt" --maxAnts 10 --id "maxAnts-10-entrada2-$counter" > logs/results-maxAnts-entrada2-maxAnts-10-$counter-run.log
+    echo "Running exp on maxAnts: 100 || Entrada2 || Counter: $counter"
+    python3 main.py --input "bases_grafos/entrada2.txt" --maxAnts 100 --id "maxAnts-100-entrada2-$counter" > logs/results-maxAnts-entrada2-maxAnts-100-$counter-run.log
 
-    echo "Running exp on maxAnts: 20 || Entrada2 || Counter: $counter"
-    python3 main.py --input "bases_grafos/entrada2.txt" --maxAnts 20 --id "maxAnts-20-entrada2-$counter" > logs/results-maxAnts-entrada2-maxAnts-20-$counter-run.log
+    echo "Running exp on maxAnts: 200 || Entrada2 || Counter: $counter"
+    python3 main.py --input "bases_grafos/entrada2.txt" --maxAnts 200 --id "maxAnts-200-entrada2-$counter" > logs/results-maxAnts-entrada2-maxAnts-200-$counter-run.log
 
 done
 echo "Done entrada2"
